84fb73737386a8d204de389db23cb228a81e2e69
phamthao0021 <24211tt0021@mail.tdc.edu.vn>
2026-08-04 18:02:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 // index.php - Trang chủ QuizTech
 require_once 'includes/config.php';
 require_once 'includes/functions.php';
